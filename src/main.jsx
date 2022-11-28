3e84a24168c9e116fdc22d4dfb0bdca866d094cc
dadsasdl <jemali.ergemlidze.1@btu.edu.ge>
2022-11-28 14:59:04 +0400

    cc

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import $ from 'jquery'
-import Popper from 'popper.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
